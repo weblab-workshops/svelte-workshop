571f2b45bff76552dc33a0311dda96f4d6fc7ec8
Enrique <enriquec@mit.edu>
2023-02-07 21:47:41 -0500

    Created resources folder

---
+++ resources/ExampleReactComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Component = () => {
+  // Script content goes here
+
+  // Styling usually has to be written in a separate file
+
+  // HTML/JSX returned here
+  return <div>This is a React component</div>;
+};
+
+export default Component;
---
+++ resources/ExampleSvelteComponent.svelte
@@ -0,0 +1,12 @@
+<script>
+  // Script goes here
+</script>
+
+<main>
+  <!-- Markup (zero or more items) goes here -->
+  This is an example Svelte component
+</main>
+
+<style>
+  /* Styles go here */
+</style>
