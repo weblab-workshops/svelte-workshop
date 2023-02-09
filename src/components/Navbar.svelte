e0b47828dad5ae6f4a7e0d52db4977bd76b4a221
Enrique <enriquec@mit.edu>
2023-02-09 02:43:54 -0500

    Added Navbar in layout

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,13 @@
+<ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="/comments">Comments</a></li>
+</ul>
+
+<style>
+  ul {
+    display: flex;
+    justify-content: space-evenly;
+    background-color: aliceblue;
+    padding: 20px;
+  }
+</style>
