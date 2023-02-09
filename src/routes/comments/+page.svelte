74d4c8a7f0748adc0a42c97ca2be3c824dfb07fe
Enrique <enriquec@mit.edu>
2023-02-09 03:07:47 -0500

    Created working form to add new comment

--- src/routes/comments/+page.svelte
+++ src/routes/comments/+page.svelte
@@ -1,9 +1,28 @@
 <script>
   import Comment from '../../components/Comment.svelte';
   import { CommentsStore } from '../../store';
+
+  let newCommentTitle = '';
+
+  const addComment = () => {
+    const newComment = {
+      id: Math.floor(Math.random() * 10000),
+      user: 'Temp',
+      title: newCommentTitle,
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, fuga.',
+    };
+
+    CommentsStore.update((prevStore) => [newComment, ...prevStore]);
+
+    newCommentTitle = '';
+  };
 </script>
 
 <h1>Comments</h1>
+<form on:submit={addComment}>
+  <input type="text" bind:value={newCommentTitle} />
+  <button type="submit">Add Comment</button>
+</form>
 <ul>
   {#each $CommentsStore as comment (comment.id)}
     <li>
