2e00d8f773154589e6dd18487e8e0cee04ade084
Enrique <enriquec@mit.edu>
2023-02-09 02:55:42 -0500

    Created basic comments page with Comment component

---
+++ src/components/Comment.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let id;
+  export let user;
+  export let title;
+  export let body;
+</script>
+
+<article>
+  <h3>
+    {user}
+  </h3>
+  <h2>{title}</h2>
+  <p>{body}</p>
+</article>
+
+<style>
+  article {
+    background-color: lightblue;
+    padding: 20px;
+    border-radius: 20px;
+    margin-bottom: 20px;
+  }
+</style>
--- src/routes/comments/+page.svelte
+++ src/routes/comments/+page.svelte
@@ -1,1 +1,21 @@
+<script>
+  import Comment from '../../components/Comment.svelte';
+
+  let comments = [
+    {
+      id: 0,
+      user: 'Username',
+      title: 'Comment',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, perferendis!',
+    },
+  ];
+</script>
+
 <h1>Comments</h1>
+<ul>
+  {#each comments as comment (comment.id)}
+    <li>
+      <Comment {...comment} />
+    </li>
+  {/each}
+</ul>
