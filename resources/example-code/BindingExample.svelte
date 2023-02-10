a3aef3d84110cde71881da59ccdae20b38a8863d
Enrique <enriquec@mit.edu>
2023-02-09 22:45:38 -0500

    Added resources and outline

---
+++ resources/example-code/BindingExample.svelte
@@ -0,0 +1,10 @@
+<!-- Copy and paste this code into the Svelte REPL (https://svelte.dev/repl) to
+see the output -->
+<script>
+  // Whatever is typed in the <input/> box
+  // will be reflected here
+  let inputValue = '';
+</script>
+
+<input type="text" bind:value={inputValue} />
+<h1>{inputValue}</h1>
---
+++ resources/example-code/EachBlockExample.svelte
@@ -0,0 +1,11 @@
+<!-- Copy and paste this code into the Svelte REPL (https://svelte.dev/repl) to
+see the output -->
+<script>
+  let items = ['a', 'b', 'c', 'd'];
+</script>
+
+<ul>
+  {#each items as item (item)}
+    <li>{item}</li>
+  {/each}
+</ul>
