8a2422c382da490d8094c6dfcf5bc36b9ff72b83
Enrique <enriquec@mit.edu>
2024-01-17 12:59:22 -0500

    Added todo

--- src/routes/comments/+page.svelte
+++ src/routes/comments/+page.svelte
@@ -1,1 +1,4 @@
 <h1>Comments</h1>
+<!-- TODO 5.4: Create a dummy array of comments data and render the comments -->
+<!-- Hint: Recall the loop rendering syntax -->
+<!-- Hint: See the Comment props and don't forget to import! -->
