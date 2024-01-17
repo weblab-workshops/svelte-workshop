1f965a46cca6d46c600981f50f68565a6a564c6a
Enrique <enriquec@mit.edu>
2024-01-17 13:07:44 -0500

    Added todos

--- src/routes/comments/+page.svelte
+++ src/routes/comments/+page.svelte
@@ -1,9 +1,14 @@
 <script>
   import Comment from '$lib/components/Comment.svelte';
   import { CommentsStore } from '$lib/stores/store';
+
+  // Hint: Create a state that tracks the current input text
+  // Hint: Create a function addComment that updates the global store
 </script>
 
 <h1>Comments</h1>
+<!-- TODO 7.2: Create a "new comment" form that adds new comments  -->
+<!-- Hint: Bind the state you created to an input tag using bind:value -->
 <ul>
   {#each $CommentsStore as comment (comment.id)}
     <li>
