9300758346b2c847a38cde1b4954d9aa6a69733f
Enrique <enriquec@mit.edu>
2024-01-17 13:02:04 -0500

    Added todos

--- src/routes/comments/+page.svelte
+++ src/routes/comments/+page.svelte
@@ -1,6 +1,8 @@
 <script>
   import Comment from '$lib/components/Comment.svelte';
+  // Hint: Don't forget to import the store!
 
+  // Hint: You can remove this state
   let comments = [
     {
       id: 0,
@@ -11,6 +13,7 @@
   ];
 </script>
 
+<!-- TODO 6.3: Replace the comments state with the global store -->
 <h1>Comments</h1>
 <ul>
   {#each comments as comment (comment.id)}
